228d76fe4a314d633c5725b7e721b280c8b9e313
Fouad <fouad@fouad-MacBookPro.(none)>
2014-01-19 18:11:25 +0100

    Login fait + éventuellement faire un bean Personne pour récupérer ses infos plus facilement ou mettre à jour le cash dans AjouterOffre

---
+++ WEB-INF/classes/Login.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.WebServlet;
+import java.sql.*;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet
+{
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+	PrintWriter out = res.getWriter();
+	res.setContentType("text/html");
+	Connection con = null;
+	
+	try
+	{
+	    Class.forName(getServletContext().getInitParameter("driver"));
+	    
+	    String url = getServletContext().getInitParameter("url");
+	    String user = getServletContext().getInitParameter("user");
+	    String mdp = getServletContext().getInitParameter("mdp");
+	    
+	    con = DriverManager.getConnection(url, user, mdp);
+	    
+	    Statement stmt = con.createStatement();
+	    
+	    PreparedStatement ps = con.prepareStatement("select * from utilisateur where login = ? and mdp = ?");
+	    ps.setString(1, req.getParameter("login"));
+	    ps.setString(2, req.getParameter("mdp"));
+	    ResultSet rs = ps.executeQuery();
+	    if (rs.next())
+	    {
+		HttpSession session = req.getSession(true);
+		session.setAttribute("iduser", rs.getString("iduser"));
+		session.setAttribute("login", rs.getString("login"));
+		session.setAttribute("nom", rs.getString("nom"));
+		session.setAttribute("prenom", rs.getString("prenom"));
+		session.setAttribute("cash", rs.getString("cash"));
+		res.sendRedirect(req.getContextPath()+"/index.jsp");
+	    }
+	    else
+	    {
+	        res.sendRedirect(req.getContextPath()+"/login.jsp");
+	    } 
+	}
+	catch (Exception e)
+	{
+	    out.println(e.getMessage());
+	}
+	finally
+	{
+	    if (con != null)
+	    {
+		try
+		{
+		    con.close();
+		}
+		catch (SQLException e)
+		{
+			out.println(e.getMessage());
+		}
+	    }
+	}
+    }
+}
---
+++ login.jsp
@@ -0,0 +1,13 @@
+<html>
+<title></title>
+    <body>
+      <center>
+      <form action="Login" method="post">
+	<p>Login : <INPUT TYPE="text" SIZE="20" NAME="login" VALUE=""> <br /></p>
+	<p>Mot de passe : <INPUT TYPE="text" SIZE="20" NAME="mdp" VALUE=""><br /></p>
+	<br />
+	<input type="submit" value="Connexion">
+	<input type="reset">
+      </form>
+    </body>
+</html>
